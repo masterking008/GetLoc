06be368e3136e3f3ac43411c2f95aed339e95aec
masterking008 <masterking008@gmail.com>
2025-01-15 19:57:17 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import HomeScreen2 from "./App";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <HomeScreen2/>
+    </View>
+  );
+}
